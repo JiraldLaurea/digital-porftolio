6bae05b6183036cb2c9581c281dec9ccd153c22e
JiraldLaurea <jiraldlaurea@gmail.com>
2023-07-22 18:09:33 +0800

    eslint rule change

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -6,15 +6,6 @@ const HeroSection = () => {
             id="home"
             className="flex flex-col justify-center h-[calc(100vh-68.4px)] bg-white dark:bg-zinc-900  border-b dark:border-b-zinc-800"
         >
-            {/* <div className="flex-none mb-6 md:mb-0 md:mr-8">
-                <Image
-                    className="border rounded-full shadow-2xl dark:shadow-none dark:border-transparent"
-                    src="/img/avatar1.jpg"
-                    alt="avatar"
-                    width={250}
-                    height={250}
-                />
-            </div> */}
             <div className="mb-8 text-center">
                 <h1 className="mb-4 text-6xl font-bold">Hi, I'm Jirald!</h1>
                 <div className="flex justify-center">
