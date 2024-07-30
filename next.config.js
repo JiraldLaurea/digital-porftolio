8b82745a1a607734049e533b581159ac08744061
JiraldLaurea <85291534+JiraldLaurea@users.noreply.github.com>
2024-07-30 11:20:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+// next.config.js
+const withPWA = require("next-pwa")({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+    // other Next.js config options
+});
