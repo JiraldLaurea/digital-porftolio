6ec8b341c59ba6a0f853dd1a6c9a2fda1e433ca9
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-18 05:03:22 +0800

    Add tab title

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -1,20 +1,21 @@
 // pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" href="/icons/icon-192x192.png" />
-          <meta name="theme-color" content="#000000" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <script>
-            {`
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>Jirald Calusay</title>
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="icon" href="/icons/icon-192x192.png" />
+                    <meta name="theme-color" content="#000000" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                    <script>
+                        {`
               if ('serviceWorker' in navigator) {
                 window.addEventListener('load', () => {
                   navigator.serviceWorker.register('/service-worker.js').then(registration => {
@@ -25,11 +26,11 @@ class MyDocument extends Document {
                 });
               }
             `}
-          </script>
-        </body>
-      </Html>
-    );
-  }
+                    </script>
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
