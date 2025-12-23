4f9a0ee6c4a90ce9acbb1c4a41a401d4c6c60dd1
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 05:00:14 +0800

    Added techstack component

---
+++ src/app/components/TechStack.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Tech } from "../Types/index";
+
+type TechStackProps = {
+    techs?: Tech[];
+};
+
+const TechStack = ({ techs }: TechStackProps) => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    return (
+        <div>
+            <h3 className="mb-1 text-xs lg:text-sm text-zinc-600 dark:text-zinc-300">
+                Tech Stack
+            </h3>
+            <div className="flex flex-wrap space-x-2 space-y-2">
+                {techs?.map((tech, index) => (
+                    <>
+                        {mounted ? (
+                            <div
+                                key={index}
+                                className="flex items-center h-8 px-2 border rounded-md lg:h-10 lg:px-3 bg-gray-50 dark:bg-primary3-dark dark:border-zinc-700"
+                            >
+                                <div className="relative w-4.5 h-4.5 mr-2 lg:w-6 lg:h-6">
+                                    <Image
+                                        src={`/img/${tech.imgSrc}`}
+                                        alt={tech.techName}
+                                        layout="fill"
+                                        objectFit="contain"
+                                    />
+                                </div>
+                                <p className="text-xs lg:text-sm text-zinc-600 dark:text-zinc-300">
+                                    {tech.techName}
+                                </p>
+                            </div>
+                        ) : (
+                            <div className="w-20 h-10 border rounded-md bg-gray-50 dark:bg-primary3-dark dark:border-zinc-700 animate-pulse" />
+                        )}
+                    </>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default TechStack;
