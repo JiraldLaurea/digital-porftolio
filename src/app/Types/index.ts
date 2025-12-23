33713768df1b0a02a4700ccfc2bbbe9353ee0ed2
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 04:58:59 +0800

    Added types

---
+++ src/app/Types/index.ts
@@ -0,0 +1,15 @@
+export type Tech = {
+    imgSrc: string;
+    techName: string;
+};
+
+export type Task = {
+    description: string;
+};
+
+export type Skill = {
+    imgSrc: string;
+    techName: string;
+    level: number;
+    mounted?: boolean;
+};
