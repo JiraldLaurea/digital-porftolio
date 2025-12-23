c8debec357a7bc8ec0f80eea7213eee5adb42fb0
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 05:02:25 +0800

    Updated herosection

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,32 +1,101 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Icon } from "@iconify/react";
 import Button from "./Button";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const HeroSection = ({ buttonsRef }: any) => {
     const resumeLink =
         "https://drive.google.com/file/d/1w-ep7j_ZsZbAQHgd_KyecjXKQGjdwqWR/view?usp=drive_link";
     const linkedInLink = "https://linkedin.com/in/jirald-calusay-064b09220";
 
+    const roles = ["Web", "Frontend", "Fullstack"];
+
+    const [text, setText] = useState("");
+    const [roleIndex, setRoleIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    const typingSpeed = 0.05; // seconds per character
+    const deletingSpeed = 0.03;
+    const pauseAfterTyping = 2;
+
+    useEffect(() => {
+        const currentRole = roles[roleIndex];
+
+        let timeout: NodeJS.Timeout;
+
+        if (!isDeleting) {
+            if (text.length < currentRole.length) {
+                timeout = setTimeout(
+                    () => setText(currentRole.slice(0, text.length + 1)),
+                    typingSpeed * 1000
+                );
+            } else {
+                timeout = setTimeout(
+                    () => setIsDeleting(true),
+                    pauseAfterTyping * 1000
+                );
+            }
+        } else {
+            if (text.length > 0) {
+                timeout = setTimeout(
+                    () => setText(currentRole.slice(0, text.length - 1)),
+                    deletingSpeed * 1000
+                );
+            } else {
+                setIsDeleting(false);
+                setRoleIndex((prev) => (prev + 1) % roles.length);
+            }
+        }
+
+        return () => clearTimeout(timeout);
+    }, [text, isDeleting, roleIndex]);
+
     return (
         <div
             id="home"
-            className="flex flex-col justify-center items-center h-[calc(100vh-68.4px)] bg-white dark:bg-[#1E263A] px-4"
+            className="flex py-10 flex-col text-center md:text-left  justify-center items-center max-h-192 min-h-fit h-[calc(100vh-64px)] bg-primary1 dark:bg-primary1-dark px-4"
         >
-            <div className="flex items-center justify-between w-full max-w-4xl mb-8 text-left">
-                <div>
-                    <h1 className="pb-1 mb-3 text-5xl font-extrabold text-transparent sm:text-7xl bg-clip-text bg-linear-to-r from-cyan-500 to-blue-500">
-                        Jirald Calusay
-                    </h1>
-                    <h3 className="mb-6 text-3xl font-bold sm:text-5xl">
-                        Assistant R&D Engineer
-                    </h3>
-                    <p className="max-w-lg mb-6 text-lg font-light sm:text-xl">
+            <div className="flex flex-col-reverse items-center justify-between w-full md:px-10 lg:px-12 md:flex-row lg:max-w-5xl">
+                <div className="mr-4 space-y-6">
+                    <div>
+                        <h1 className="mb-4 text-3xl font-semibold leading-tight sm:text-4xl lg:text-6xl">
+                            Jirald Calusay
+                        </h1>
+                        <div className="flex items-center justify-center text-4xl font-extrabold sm:text-5xl md:justify-start lg:text-7xl">
+                            <h3 className="text-accent dark:text-accent-dark">
+                                <motion.span
+                                    key={text}
+                                    initial={{ opacity: 1 }}
+                                    animate={{ opacity: 1 }}
+                                    transition={{ duration: 1 }}
+                                >
+                                    {text}
+                                </motion.span>
+                            </h3>
+                            <motion.span
+                                className="font-normal"
+                                animate={{ opacity: [1, 0, 1] }}
+                                transition={{
+                                    duration: 1,
+                                    repeat: Infinity,
+                                }}
+                            >
+                                |
+                            </motion.span>
+                            <span className="text-current">Developer</span>
+                        </div>
+                    </div>
+
+                    <p className="max-w-lg md:text-lg lg:text-xl">
                         Passionate about developing user-friendly websites and
-                        creating seamless user experiences.
+                        creating seamless user experiences
                     </p>
-                    <div ref={buttonsRef} className="flex space-x-4">
+                    <div
+                        ref={buttonsRef}
+                        className="flex justify-center space-x-4 md:justify-start"
+                    >
                         <Button link={resumeLink} text="Resume" isResume />
                         <Button
                             link={linkedInLink}
@@ -36,7 +105,7 @@ const HeroSection = ({ buttonsRef }: any) => {
                     </div>
                 </div>
 
-                <div className="relative w-64 h-64 overflow-hidden rounded-full border dark:border-0">
+                <div className="relative flex-none mb-4 overflow-hidden border rounded-full md:mb-0 w-30 h-30 sm:w-44 sm:h-44 lg:w-56 lg:h-56 dark:border-0">
                     <Image
                         src={`/img/avatar2.jpg`}
                         alt=""
