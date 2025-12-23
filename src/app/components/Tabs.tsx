971b5949a807ad495872cef90ff1b6dbe8996dfd
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 05:00:32 +0800

    Update tabs

--- src/app/components/Tabs.tsx
+++ src/app/components/Tabs.tsx
@@ -12,26 +12,36 @@ const Tabs = ({
     categories,
     activeTab,
     setActiveTab,
+    isProjects,
     isAlternate,
 }: TabsProps) => (
-    <div className={`flex overflow-x-auto justify-normal`}>
+    <div className={`flex justify-center overflow-x-auto`}>
         <div
-            className={`flex p-2 mb-6 space-x-2 overflow-x-auto border dark:border-zinc-700 rounded-full w-fit ${
-                isAlternate ? "border-zinc-600" : "border"
+            className={`flex p-2 pb-0 mb-6 w-fit overflow-x-auto justify-center bg-primary1 border dark:border-zinc-700 dark:bg-primary1-dark rounded-md ${
+                isAlternate ? "border-zinc-600" : ""
             } `}
         >
             {categories.map((category, index) => (
-                <button
-                    key={index}
-                    className={`${
-                        activeTab === index
-                            ? "bg-blue-500 dark:bg-blue-600 dark:hover:bg-blue-600 text-white"
-                            : "dark:hover:bg-slate-700 hover:bg-slate-200"
-                    } py-2 px-4 rounded-full`}
-                    onClick={() => setActiveTab(index)}
-                >
-                    {category}
-                </button>
+                <div className="flex flex-col items-center space-y-1.5">
+                    <button
+                        key={index}
+                        className={`transition-colors rounded-md hover:bg-zinc-100 dark:hover:bg-primary3-dark ${
+                            activeTab === index
+                                ? "dark:text-white"
+                                : "text-secondary-text dark:text-secondary-text-dark"
+                        } py-2 px-4`}
+                        onClick={() => setActiveTab(index)}
+                    >
+                        {category}
+                    </button>
+                    <div
+                        className={`h-0.5 rounded-full w-full transition-colors ${
+                            activeTab === index
+                                ? "bg-black dark:bg-white"
+                                : "bg-transparent"
+                        }`}
+                    ></div>
+                </div>
             ))}
         </div>
     </div>
