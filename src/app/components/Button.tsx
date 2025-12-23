294b1884e650f1964e2e0fba117963235251132b
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 05:03:43 +0800

    Updated button component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type ButtonProps = {
     link: string;
     text: string;
-    isResume: boolean;
+    isResume?: boolean;
     isSmaller?: boolean;
 };
 
@@ -14,13 +14,17 @@ const Button = ({ link, text, isResume, isSmaller }: ButtonProps) => {
             href={link}
             target="_blank"
             className={`
-                flex items-center px-6 ${
-                    isSmaller ? "py-3" : "py-3"
-                }  space-x-1 rounded-md transition-colors  ${
-                isResume
-                    ? "text-white bg-blue-500 hover:bg-blue-600 dark:hover:bg-blue-700 dark:bg-blue-600 dark:text-white dark:border-transparent"
-                    : "text-blue-500 bg-white border hover:border-blue-500 dark:hover:border-blue-500 dark:bg-transparent dark:text-blue-500 dark:border-zinc-700 dark:border-opacity-50"
-            }`}
+                flex items-center rounded-md transition-colors 
+                ${
+                    isSmaller
+                        ? "px-4 text-sm h-10"
+                        : "px-4 sm:px-6 h-10 text-sm sm:text-base sm:h-12"
+                } 
+                ${
+                    isResume
+                        ? "text-white bg-accent hover:bg-accent-hovered dark:hover:bg-accent-hovered-dark dark:bg-accent-dark"
+                        : "border hover:bg-gray-100 hover:dark:bg-primary3-dark dark:border-zinc-700"
+                }`}
         >
             <p>{text}</p>
         </a>
