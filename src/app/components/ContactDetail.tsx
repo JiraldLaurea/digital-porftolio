e74cb4427c5a37bad321d860c3a7b4b0f309292e
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 11:30:03 +0800

    Update ContactDetail

--- src/app/components/ContactDetail.tsx
+++ src/app/components/ContactDetail.tsx
@@ -1,18 +1,17 @@
 "use client";
 import { Icon } from "@iconify/react";
-import React from "react";
 
 const ContactDetail = ({ icon, header, detail }: any) => {
     return (
         <div className="flex items-center space-x-3">
-            <div className="flex items-center justify-center w-10 h-10 text-white rounded-full bg-accent dark:bg-accent-dark">
-                <Icon width={20} height={20} icon={icon} />
+            <div className="flex items-center justify-center text-white rounded-full w-9 h-9 sm:w-10 sm:h-10 bg-accent dark:bg-accent-dark">
+                <Icon className="w-5 h-5" icon={icon} />
             </div>
             <div className="text-left">
-                <p className="text-sm text-zinc-600 dark:text-zinc-300">
+                <p className="text-xs xs:text-sm text-zinc-600 dark:text-zinc-300">
                     {header}
                 </p>
-                <p className="">{detail}</p>
+                <p className="text-sm xs:text-base">{detail}</p>
             </div>
         </div>
     );
