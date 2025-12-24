ebb1041346e2b81e2258e8c663b6feaa7621ad76
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 11:30:55 +0800

    Update Experience

--- src/app/components/ExperienceCard.tsx
+++ src/app/components/ExperienceCard.tsx
@@ -1,5 +1,5 @@
+import { Task, Tech } from "../Types/index";
 import TechStack from "./TechStack";
-import { Tech, Task } from "../Types/index";
 
 type ExperienceCardProps = {
     project: string;
@@ -18,12 +18,12 @@ const ExperienceCard = ({
 }: ExperienceCardProps) => {
     return (
         <div
-            className={`flex flex-col p-6 space-y-5 bg-white border border-t-0 dark:bg-primary1-dark dark:border-zinc-700 ${
+            className={`flex flex-col p-4 sm:p-6 space-y-5 bg-white border border-t-0 dark:bg-primary1-dark dark:border-zinc-700 ${
                 isLast && "rounded-b-md"
             }`}
         >
             <div>
-                <h2 className="text-lg font-medium">{project}</h2>
+                <h2 className="font-medium sm:text-lg">{project}</h2>
                 <h3 className="text-sm text-zinc-600 dark:text-zinc-300">
                     {date}
                 </h3>
--- src/app/components/ExperienceJob.tsx
+++ src/app/components/ExperienceJob.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 
 type ExperienceJobProps = {
     imgSrc: string;
@@ -20,35 +19,33 @@ const ExperienceJob = ({
 }: ExperienceJobProps) => {
     return (
         <div
-            className={`flex border dark:border-zinc-700 bg-white dark:bg-primary1-dark rounded-t-md  p-6 py-4 items-center gap-2 ${
+            className={`flex border dark:border-zinc-700 bg-white dark:bg-primary1-dark rounded-t-md p-4  sm:p-6 sm:py-4 items-center gap-2 ${
                 isFirst ? "" : "mt-6"
             }`}
         >
             <div
-                className={`rounded-full ${
+                className={`rounded-full h-16 w-16 sm:h-20 sm:w-20 ${
                     hasNoBackground
                         ? "p-0 bg-transparent"
-                        : "h-20 w-20 flex items-center justify-center bg-white border"
+                        : "p-2 sm:p-4 sm:w-20 flex items-center justify-center bg-white border"
                 }`}
             >
-                <Image
-                    className=""
-                    src={`/img/${imgSrc}`}
-                    alt=""
-                    width={hasNoBackground ? 80 : 50}
-                    height={hasNoBackground ? 80 : 50}
-                    unoptimized
-                    style={{
-                        maxWidth: "100%",
-                        height: "auto"
-                    }} />
+                <div className="relative w-full h-full ">
+                    <Image
+                        className=""
+                        src={`/img/${imgSrc}`}
+                        alt={company}
+                        quality={100}
+                        fill
+                    />
+                </div>
             </div>
             <div className="flex flex-col">
-                <h2 className="text-xl font-medium">{job}</h2>
-                <p className=" text-zinc-600 dark:text-secondary-text-dark">
+                <h2 className="font-medium xs:text-lg sm:text-xl">{job}</h2>
+                <p className="text-xs xs:text-sm text-zinc-600 sm:text-base dark:text-secondary-text-dark">
                     {company}
                 </p>
-                <p className="text-sm text-zinc-600 dark:text-secondary-text-dark">
+                <p className="text-xs xs:text-sm text-zinc-600 dark:text-secondary-text-dark">
                     {date}
                 </p>
             </div>
