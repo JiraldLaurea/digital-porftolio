626542be61b83e898cbe4984a8b004ee77170d81
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-25 03:14:47 +0800

    Update head and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 "use client";
 import "./globals.css";
-import { Inter_Tight } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-
-const inter = Inter_Tight({
-    subsets: ["latin"],
-});
 
 export default function RootLayout({
     children,
@@ -14,9 +9,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body
-                className={`${inter.className} bg-white dark:bg-primary1-dark`}
-            >
+            <body className="bg-white dark:bg-primary1-dark">
                 <ThemeProvider
                     enableSystem={true}
                     attribute="class"
