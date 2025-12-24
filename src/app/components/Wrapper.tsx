711892e84568bfaf2f89f25040c92b644c750f9d
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-24 11:41:12 +0800

    Update Wrapper

--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -26,24 +26,27 @@ const Wrapper = ({
     return (
         <div
             id={id}
-            className={`flex flex-col border-t items-center py-10 dark:border-zinc-700 ${
+            className={`flex flex-col border-t items-center py-10 dark:border-zinc-700 
+            ${
                 isAlternate
                     ? "bg-primary1 dark:bg-primary1-dark"
                     : "bg-primary2 dark:bg-primary2-dark"
-            } ${
+            } 
+            ${
                 isContact &&
                 "justify-center min-h-fit max-h-192 h-[calc(100vh-64px)]"
-            } ${isFullHeight && "min-h-fit h-[calc(100vh-64px)] max-h-192"}
-             ${isPoject && "px-4 sm:px-18"}`}
+            } 
+            ${isFullHeight && "min-h-fit h-[calc(100vh-64px)] max-h-192"}
+            ${isPoject && "px-4 sm:px-18"}`}
         >
             <div
                 className={`flex flex-col w-full max-w-6xl px-4 mb-8 ${
                     isCentered && "text-center"
                 }`}
             >
-                <p className="text-3xl font-semibold md:text-3xl">{title}</p>
+                <p className="text-2xl font-semibold sm:text-3xl">{title}</p>
                 <p
-                    className={`text-lg text-secondary-text dark:text-secondary-text-dark`}
+                    className={`sm:text-lg text-secondary-text dark:text-secondary-text-dark`}
                 >
                     {subtitle}
                 </p>
