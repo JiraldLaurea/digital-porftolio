452c91b5f4a1a88220039273ba2f7c0f0c2ad032
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-25 17:45:37 +0800

    Update Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,30 +5,36 @@ type Props = {};
 const Footer = (props: Props) => {
     return (
         <div className="flex justify-center py-10 border-t bg-gray-50 dark:bg-primary2-dark dark:border-zinc-700">
-            <div className="flex items-center space-x-8 text-sm sm:text-base">
-                <a
-                    href="https://www.linkedin.com/in/jirald-calusay-064b09220"
-                    target="_blank"
-                    className="flex items-center space-x-1"
-                >
-                    <Icon
-                        icon="mdi:linkedin"
-                        className="w-8 h-8 cursor-pointer sm:w-10 sm:h-10"
-                    />
-                    <p>LinkedIn</p>
-                </a>
-                <a
-                    href="https://github.com/JiraldLaurea"
-                    target="_blank"
-                    className="flex items-center space-x-1"
-                >
-                    <Icon
-                        icon="mdi:github"
-                        className="w-8 h-8 cursor-pointer sm:w-10 sm:h-10"
-                    />
-                    <p>Github</p>
-                </a>
+            <div className="flex flex-col-reverse items-center justify-center w-full max-w-6xl px-4 sm:justify-between sm:flex-row">
+                <p className="text-sm text-secondary-text dark:text-secondary-text-dark">
+                    © 2025 Jirald Calusay
+                </p>
+                <div className="flex items-center mb-4 space-x-4 text-sm sm:space-x-8 sm:mb-0 sm:text-base">
+                    <a
+                        href="https://www.linkedin.com/in/jirald-calusay-064b09220"
+                        target="_blank"
+                        className="flex items-center space-x-1"
+                    >
+                        <Icon
+                            icon="mdi:linkedin"
+                            className="w-8 h-8 cursor-pointer sm:w-9 sm:h-9"
+                        />
+                        <p>LinkedIn</p>
+                    </a>
+                    <a
+                        href="https://github.com/JiraldLaurea"
+                        target="_blank"
+                        className="flex items-center space-x-1"
+                    >
+                        <Icon
+                            icon="mdi:github"
+                            className="w-8 h-8 cursor-pointer sm:w-9 sm:h-9"
+                        />
+                        <p>Github</p>
+                    </a>
+                </div>
             </div>
+            <div />
         </div>
     );
 };
