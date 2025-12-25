82caea2d07f76d32c430f87db7b271ca89e47d58
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-25 16:38:44 +0800

    Fix PWA integration

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@
 // next.config.js
 const withPWA = require("next-pwa")({
     dest: "public", // where service worker and files will be output
-    disable: process.env.NODE_ENV === "development", // disables PWA in dev
-    register: true, // automatically register service worker
-    skipWaiting: true, // activate new SW immediately
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    scope: "/app",
+    sw: "service-worker.js",
 });
 
 module.exports = withPWA({
