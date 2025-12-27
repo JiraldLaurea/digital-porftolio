d2d8e2af9fc5dd69ac53f4f3681f8061979cb8fd
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-28 01:32:12 +0800

    Update Tabs

--- src/app/components/Tabs.tsx
+++ src/app/components/Tabs.tsx
@@ -1,5 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { motion } from "framer-motion";
 
 type TabsProps = {
     categories: string[];
@@ -16,8 +15,6 @@ const Tabs = ({
     isProject,
     section,
 }: TabsProps) => {
-    const [hoveredTab, setHoveredTab] = useState<number | null>(null);
-
     const transition: any = {
         type: "tween",
         ease: "easeOut",
@@ -30,18 +27,16 @@ const Tabs = ({
                 isProject ? "justify-center" : "justify-start"
             }`}
         >
-            <div className="flex p-2 pb-0 border rounded-3xl bg-primary1 dark:border-zinc-700 dark:bg-primary1-dark">
+            <div className="flex p-2 border rounded-full bg-primary1 dark:border-zinc-700 dark:bg-primary1-dark">
                 <div className="z-0 flex items-center pb-0 overflow-x-auto overflow-y-hidden">
                     {categories.map((category, index) => (
                         <div
                             key={index}
-                            className="flex relative flex-col items-center space-y-1.5"
+                            className="relative flex flex-col items-center"
                         >
                             <motion.button
-                                onHoverStart={() => setHoveredTab(index)}
-                                onHoverEnd={() => setHoveredTab(null)}
                                 onClick={() => setActiveTab(index)}
-                                className={` relative mb-2 rounded-3xl py-2 px-4 sm:hover:dark:text-white sm:hover:text-black transition-colors ${
+                                className={` relative rounded-full py-2 px-4 sm:hover:dark:text-white sm:hover:text-black transition-colors ${
                                     activeTab === index
                                         ? "dark:text-white"
                                         : "text-secondary-text dark:text-secondary-text-dark"
@@ -50,26 +45,6 @@ const Tabs = ({
                                 {category}
                             </motion.button>
 
-                            <AnimatePresence>
-                                {hoveredTab === index ? (
-                                    <motion.span
-                                        layoutId={"hover"}
-                                        className="absolute inset-0 hidden h-10 rounded-3xl sm:inline bg-primary-hovered dark:bg-primary-hovered-dark"
-                                        style={{ zIndex: -1 }}
-                                        initial={{
-                                            opacity: 0,
-                                        }}
-                                        animate={{
-                                            opacity: 1,
-                                        }}
-                                        exit={{
-                                            opacity: 0,
-                                        }}
-                                        transition={transition}
-                                    />
-                                ) : null}
-                            </AnimatePresence>
-
                             {activeTab === index && (
                                 <motion.span
                                     layoutId={
@@ -77,12 +52,15 @@ const Tabs = ({
                                             ? "skillsUnderline"
                                             : "projectsUnderline"
                                     }
-                                    className="absolute w-[calc(100%-32px)] left-4 rounded-full right-0 bottom-2 h-0.5 bg-black dark:bg-white"
-                                    transition={{
-                                        type: "spring",
-                                        stiffness: 2000,
-                                        damping: 100,
+                                    className="absolute inset-0 border rounded-full dark:border-zinc-700 bg-primary3 dark:bg-primary3-dark"
+                                    style={{ zIndex: -1 }}
+                                    animate={{
+                                        opacity: 1,
                                     }}
+                                    exit={{
+                                        opacity: 0,
+                                    }}
+                                    transition={transition}
                                 />
                             )}
                         </div>
