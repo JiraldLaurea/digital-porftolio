ae75b003a07b696548590849019669f6b08f2770
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-28 10:43:32 +0800

    Update types

--- src/app/Types/index.ts
+++ src/app/Types/index.ts
@@ -13,3 +13,17 @@ export type Skill = {
     level: number;
     mounted?: boolean;
 };
+
+export type ProjectType = "Website" | "UI Design";
+
+export type Project = {
+    projectType: ProjectType;
+    imgSrc: string;
+    name: string;
+    description: string;
+    link?: string;
+    sourceCode?: string;
+    figmaLink?: string;
+    techs?: Tech[];
+    index?: number;
+};
