612ea9dd23b09503ca2be395ea06d1c1dab9202f
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-17 08:50:45 +0800

    refactor: apply font frorm next/font/google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 "use client";
+import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { Inter_Tight } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter_Tight({ subsets: ["latin"] });
 
 export default function RootLayout({
     children,
@@ -9,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="en" suppressHydrationWarning className={inter.className}>
             <body className="bg-white dark:bg-primary1-dark">
                 <ThemeProvider
                     enableSystem={true}
