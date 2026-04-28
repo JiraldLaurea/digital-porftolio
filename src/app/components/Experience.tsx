e034007b1e2f4cc9312800b504c235ef4dc87e24
JiraldLaurea <jiraldlaurea@gmail.com>
2026-04-29 05:23:35 +0800

    chore: update resume info and experience details

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -13,9 +13,9 @@ const Experience = () => {
         >
             <ExperienceJob
                 imgSrc="exp_aws.png"
-                job="Assistant R&D Engineer"
+                job="Frontend / React Developer"
                 company="Advanced World Solutions, Inc."
-                date="October 2023 - Present"
+                date="October 2023 - March 2026"
                 isFirst
             />
             <div className="p-4 bg-white border-b border-x rounded-b-3xl sm:p-6 dark:bg-primary1-dark dark:border-zinc-700">
